6850af4d015b9575c3637cccbdfff0b386ab8f95
Renato Arrieche <rca.zero@gmail.com>
2022-11-14 18:50:05 -0300

    faltou um detalhe

--- odm_classes.py
+++ odm_classes.py
@@ -7,10 +7,20 @@ Created on Mon Nov 14 12:03:39 2022
 
 import mongoengine as me
 import datetime
+import pandas as pd
+
+def queryset_pandas(queryset):
+    return pd.DataFrame(list(queryset))
+    pass
 
 class DatasetQuerySet(me.QuerySet):
     pass
 
 class DataOriginQuerySet(me.QuerySet):
     pass
+
+class TagQuerySet(me.QuerySet):
+    pass
+
+
 
